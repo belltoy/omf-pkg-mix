db77d279a9af837f84cf716450d0ae6efd284ef9
belltoy <belltoy@gmail.com>
2015-11-27 14:40:27 +0800

    First commit

---
+++ mix.fish
@@ -0,0 +1,18 @@
+# SYNOPSIS
+#   mix [options]
+#
+# USAGE
+#   Options
+#
+
+function init -a path --on-event init_mix
+
+end
+
+# function mix -d "My package"
+#
+# end
+#
+# function uninstall --on-event uninstall_mix
+#
+# end
